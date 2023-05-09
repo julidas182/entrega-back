ba477cc2a695160275c6ea74556f27a2ca60c3d0
julidas <juliandascenzo@gmail.com>
2023-05-09 00:31:58 -0300

    repo2

--- enntregable1.js
+++ enntregable1.js
@@ -1,15 +1,22 @@
 class ProductManager {
+    #products
     constructor() {
-        this.products = []
+        
+        this.#products = []
     }
 
-    getProducts = () => this.products
+    getProducts = () => this.#products
+
+    creadorId = () => {
+        return (this.#products.length === 0) ? 1 : this.#products[this.#products.length-1].id + 1
+    }
 
     addProducts = (title, description, price) => {
-        this.products.push({title, description, price})
+        const id = 
+        this.#products.push({id: this.creadorId(), title, description, price})
     }
 }
 
 const productManager = new ProductManager()
 productManager.addProducts('motorolla g50', 'gama media', 50000)
-console.log(productManager.getProducts);
+console.log(productManager.getProducts());
